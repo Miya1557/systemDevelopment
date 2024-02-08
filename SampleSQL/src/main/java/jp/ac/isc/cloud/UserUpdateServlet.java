77fe6a929339df2ae1c9726ede5e6aa764f0da98
miya1 <miya1@GALLERIA_XT.mshome.net>
2024-02-09 07:37:37 +0900

    練習02_2

---
+++ SampleSQL/src/main/java/jp/ac/isc/cloud/UserUpdateServlet.java
@@ -0,0 +1,34 @@
+package jp.ac.isc.cloud;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+@WebServlet("/UserUpdateServlet")
+public class UserUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection users = null;
+		try {
+			request.setCharacterEncoding("utf-8");
+			users = DBConnection.openConnection();
+			String id = request.getParameter("updateId");
+			String name = request.getParameter("updateName");
+			Statement state = users.createStatement();
+			state.executeUpdate("UPDATE user_table SET name='" + name + "' WHERE id ='" + id + "'");
+			DBConnection.closeConnection(users, state);
+			response.sendRedirect("/select");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
